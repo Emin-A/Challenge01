04a0172f2509f36ab65f6258d0cc303724184459
Emin-A <72197800+Emin-A@users.noreply.github.com>
2020-10-09 16:53:56 +0200

    DOM

---
+++ Challenge01.js
@@ -0,0 +1,9 @@
+function testOne() {
+    const td = document.querySelectorAll("td")
+    td.innerText = "Music Instruments"
+}
+
+function headOne() {
+    const h1 = document.querySelector("h1")
+    h1.innerText = "Best Guitars!"
+}
